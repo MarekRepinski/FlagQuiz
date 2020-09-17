eeb3f5d02d73ee5ba55455236e2425e3ee5d8d34
Marek <repinski@hotmail.com>
2020-09-17 21:16:08 +0200

    Miner design changes

---
+++ app/src/main/java/se/ctescape/flagquiz/GameEndActivity.kt
@@ -0,0 +1,11 @@
+package se.ctescape.flagquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameEndActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_end)
+    }
+}
