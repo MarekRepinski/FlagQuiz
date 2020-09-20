fb15d6e9540e86abd1a86cffceac47c15aedc80a
Marek <repinski@hotmail.com>
2020-09-20 21:02:30 +0200

    Change of plans. Make the gamw page and the end page with fragments. This took a hole day of work!!!!!

---
+++ app/src/main/java/se/ctescape/flagquiz/FragmentMainGame.kt
@@ -0,0 +1,142 @@
+package se.ctescape.flagquiz
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_main_game.*
+
+private const val ARG_PARAM1 = "param1"
+
+class FragmentMainGame : Fragment() {
+    val gameImageViews = mutableListOf<ImageView>()
+    lateinit var flagQuizGame: FlagQuiz
+    var onlyOnePick = true
+
+    private lateinit var areas: BooleanArray
+    lateinit var rubrik : TextView
+    lateinit var question : TextView
+    private var areaTemp = BooleanArray(0)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            areas = it.getBooleanArray(ARG_PARAM1)?:areaTemp
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_main_game, container, false)
+        rubrik = v.findViewById(R.id.tvGameHeader)
+        question = v.findViewById(R.id.tvGameQuestion)
+        var id = 0
+        for (i in 0..3) {
+            val tempStr = "ivFlagg$i"
+            id = resources.getIdentifier("ivFlagg$i", "id", activity!!.packageName)
+//            Log.d("AIK",id.toString())
+            gameImageViews.add(v.findViewById<ImageView>(id))
+            gameImageViews[i].setOnTouchListener { v, e ->
+                getAnswer(v, e, gameImageViews[i], i)
+                true
+            }
+        }
+        flagQuizGame = FlagQuiz(areas)
+        if (!flagQuizGame.checkFlagsLeft())
+            endGame()
+        else
+//            Log.d("AIK","${R.string.questNo}, ${this.flagQuizGame.rond.toString()}, ${this.flagQuizGame.noOfFlags().toString()}")
+            printNewFlags()
+        return v
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: BooleanArray) =
+            FragmentMainGame().apply {
+                arguments = Bundle().apply {
+                    putBooleanArray(ARG_PARAM1, param1)
+                }
+            }
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    fun printNewFlags() {
+        val idArray = flagQuizGame.printFlags()
+        rubrik.text = activity!!.getString(
+            R.string.questNo,
+            this.flagQuizGame.rond.toString(),
+            this.flagQuizGame.noOfFlags().toString()
+        )
+        for (i in 0..(gameImageViews.size - 1)) {
+//            Log.d("AIK",idArray[i])
+            gameImageViews[i].setImageResource(
+                resources.getIdentifier(
+                    idArray[i],
+                    "drawable",
+                    activity!!.packageName
+                )
+            )
+        }
+        question.text = getString(R.string.gameQuestion, idArray[gameImageViews.size])
+        onlyOnePick = true
+    }
+
+    fun endGame() {
+        Toast.makeText(activity, "Game ended", Toast.LENGTH_SHORT).show()
+    }
+
+    fun getAnswer(v: View, e: MotionEvent, iv: ImageView, i: Int) {
+        if (onlyOnePick) {
+            when (e.action) {
+                MotionEvent.ACTION_DOWN -> {
+//                    Log.d("AIK", "Down")
+                    iv.setImageResource(R.drawable.smileyworried)
+                }
+                MotionEvent.ACTION_UP -> {
+                    onlyOnePick = false
+//                    Log.d("AIK", "Up - $i right answer = ${flagQuizGame.correctAnswer}")
+                    if (i != flagQuizGame.correctAnswer) {
+//                        Log.d("AIK", "Up - wrong")
+                        iv.setImageResource(R.drawable.wrong)
+                    } else {
+//                        Log.d("AIK", "Up - correct")
+                        iv.setImageResource(R.drawable.correct)
+                    }
+//                TODO: Detta funkar! dålig lösning. Titta på Coroutines
+                    Handler().postDelayed({
+//                        Log.d("AIK", "in timer")
+                        if (i != flagQuizGame.correctAnswer || !flagQuizGame.checkFlagsLeft())
+                            endGame()
+                        else
+                            printNewFlags()
+                    }, 2000)
+//                Funkar inte!!
+//                GlobalScope.launch { // launch a new coroutine in background and continue
+//                    delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+//                    CoroutineScope(Dispatchers.Main).launch {
+//                        Log.d("AIK", "in timer")
+//                        resetImages()
+//                    }
+//                }
+//                Funkar inte!!
+//                Timer().schedule(3000){
+//                    Log.d("AIK","in timer")
+//                    resetImages()
+//                }
+//                    Log.d("AIK", "after timer")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/se/ctescape/flagquiz/GameActivity.kt
+++ app/src/main/java/se/ctescape/flagquiz/GameActivity.kt
@@ -13,25 +13,12 @@ import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
     val gameImageViews = mutableListOf<ImageView>()
-    lateinit var flagQuizGame: FlagQuiz
-    var onlyOnePick = true
+//    lateinit var flagQuizGame: FlagQuiz
+//    var onlyOnePick = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        var id = 0
-//        Log.d("AIK", "id = $id")
-        for (i in 0..3) {
-            val tempStr = "ivFlagg$i"
-//            Log.d("AIK", "id = $id")
-            id = resources.getIdentifier("ivFlagg$i", "id", packageName)
-//            Log.d("AIK", "id = $id")
-            gameImageViews.add(findViewById<ImageView>(id))
-            gameImageViews[i].setOnTouchListener { v, e ->
-                getAnswer(v, e, gameImageViews[i], i)
-                true
-            }
-        }
 
         val noOfBools = intent.getIntExtra("noOfBools", 0)
         if (noOfBools > 0) {
@@ -39,83 +26,95 @@ class GameActivity : AppCompatActivity() {
             for (i in 0..(noOfBools - 1)) {
                 areas[i] = intent.getBooleanExtra("bool$i", false)
             }
-            if (areas[1])
-                areas[0] = true
-            flagQuizGame = FlagQuiz(areas)
-            if (!flagQuizGame.checkFlagsLeft())
-                endGame()
-            else
-                printNewFlags()
+            val f = FragmentMainGame.newInstance(areas)
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.frmMainGame, f, "gameFragment")
+            transaction.commit()
         }
+
+//        var id = 0
+////        Log.d("AIK", "id = $id")
+//        for (i in 0..3) {
+//            val tempStr = "ivFlagg$i"
+////            Log.d("AIK", "id = $id")
+//            id = resources.getIdentifier("ivFlagg$i", "id", packageName)
+////            Log.d("AIK", "id = $id")
+//            gameImageViews.add(findViewById<ImageView>(id))
+//            gameImageViews[i].setOnTouchListener { v, e ->
+//                getAnswer(v, e, gameImageViews[i], i)
+//                true
+//            }
+//        }
+//
     }
 
-    @SuppressLint("StringFormatInvalid")
-    fun printNewFlags() {
-        val idArray = flagQuizGame.printFlags()
-        tvGameHeader.text = getString(
-            R.string.questNo,
-            this.flagQuizGame.rond.toString(),
-            this.flagQuizGame.noOfFlags().toString()
-        )
-        for (i in 0..(gameImageViews.size - 1)) {
-//            Log.d("AIK",idArray[i])
-            gameImageViews[i].setImageResource(
-                resources.getIdentifier(
-                    idArray[i],
-                    "drawable",
-                    packageName
-                )
-            )
-        }
-        tvGameQuestion.text = getString(R.string.gameQuestion, idArray[gameImageViews.size])
-        onlyOnePick = true
-    }
-
-    fun endGame() {
-        Toast.makeText(this, "Game ended", Toast.LENGTH_SHORT).show()
-    }
-
-    fun getAnswer(v: View, e: MotionEvent, iv: ImageView, i: Int) {
-        if (onlyOnePick) {
-            when (e.action) {
-                MotionEvent.ACTION_DOWN -> {
-//                    Log.d("AIK", "Down")
-                    iv.setImageResource(R.drawable.smileyworried)
-                }
-                MotionEvent.ACTION_UP -> {
-                    onlyOnePick = false
-//                    Log.d("AIK", "Up - $i right answer = ${flagQuizGame.correctAnswer}")
-                    if (i != flagQuizGame.correctAnswer) {
-//                        Log.d("AIK", "Up - wrong")
-                        iv.setImageResource(R.drawable.wrong)
-                    } else {
-//                        Log.d("AIK", "Up - correct")
-                        iv.setImageResource(R.drawable.correct)
-                    }
-//                TODO: Detta funkar! dålig lösning. Titta på Coroutines
-                    Handler().postDelayed({
-//                        Log.d("AIK", "in timer")
-                        if (i != flagQuizGame.correctAnswer || !flagQuizGame.checkFlagsLeft())
-                            endGame()
-                        else
-                            printNewFlags()
-                    }, 2000)
-//                Funkar inte!!
-//                GlobalScope.launch { // launch a new coroutine in background and continue
-//                    delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-//                    CoroutineScope(Dispatchers.Main).launch {
-//                        Log.d("AIK", "in timer")
-//                        resetImages()
+//    @SuppressLint("StringFormatInvalid")
+//    fun printNewFlags() {
+//        val idArray = flagQuizGame.printFlags()
+//        tvGameHeader.text = getString(
+//            R.string.questNo,
+//            this.flagQuizGame.rond.toString(),
+//            this.flagQuizGame.noOfFlags().toString()
+//        )
+//        for (i in 0..(gameImageViews.size - 1)) {
+////            Log.d("AIK",idArray[i])
+//            gameImageViews[i].setImageResource(
+//                resources.getIdentifier(
+//                    idArray[i],
+//                    "drawable",
+//                    packageName
+//                )
+//            )
+//        }
+//        tvGameQuestion.text = getString(R.string.gameQuestion, idArray[gameImageViews.size])
+//        onlyOnePick = true
+//    }
+//
+//    fun endGame() {
+//        Toast.makeText(this, "Game ended", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun getAnswer(v: View, e: MotionEvent, iv: ImageView, i: Int) {
+//        if (onlyOnePick) {
+//            when (e.action) {
+//                MotionEvent.ACTION_DOWN -> {
+////                    Log.d("AIK", "Down")
+//                    iv.setImageResource(R.drawable.smileyworried)
+//                }
+//                MotionEvent.ACTION_UP -> {
+//                    onlyOnePick = false
+////                    Log.d("AIK", "Up - $i right answer = ${flagQuizGame.correctAnswer}")
+//                    if (i != flagQuizGame.correctAnswer) {
+////                        Log.d("AIK", "Up - wrong")
+//                        iv.setImageResource(R.drawable.wrong)
+//                    } else {
+////                        Log.d("AIK", "Up - correct")
+//                        iv.setImageResource(R.drawable.correct)
 //                    }
+////                TODO: Detta funkar! dålig lösning. Titta på Coroutines
+//                    Handler().postDelayed({
+////                        Log.d("AIK", "in timer")
+//                        if (i != flagQuizGame.correctAnswer || !flagQuizGame.checkFlagsLeft())
+//                            endGame()
+//                        else
+//                            printNewFlags()
+//                    }, 2000)
+////                Funkar inte!!
+////                GlobalScope.launch { // launch a new coroutine in background and continue
+////                    delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+////                    CoroutineScope(Dispatchers.Main).launch {
+////                        Log.d("AIK", "in timer")
+////                        resetImages()
+////                    }
+////                }
+////                Funkar inte!!
+////                Timer().schedule(3000){
+////                    Log.d("AIK","in timer")
+////                    resetImages()
+////                }
+////                    Log.d("AIK", "after timer")
 //                }
-//                Funkar inte!!
-//                Timer().schedule(3000){
-//                    Log.d("AIK","in timer")
-//                    resetImages()
-//                }
-//                    Log.d("AIK", "after timer")
-                }
-            }
-        }
-    }
+//            }
+//        }
+//    }
 }
